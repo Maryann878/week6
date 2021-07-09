c6e9b5acef2d7910c8f2e8d2de7c2a49caa40864
Maryann878 <85343201+Maryann878@users.noreply.github.com>
2021-07-09 12:00:44 +0100

    Add files via upload

---
+++ task.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+// const password1 = document.form.p1;
+// const password2 = document.form.p2;
+
+// const submitBtn = document.querySelector('#submit');
+
+// submitBtn.addEventListener('click', function (e) {
+//  e.preventDefault();
+// if (password1.value !== password2.value) {
+//     alert('passwords does not match');
+// return false;
+//  } 
+//  else {
+//      alert('form Submitted');
+// }
+// }
+// );
+// console.log(password1);
+
+
+const check = document.forms.checkNumber;
+
+const submitChecks = document.querySelector('#checkS');
+submitChecks.addEventListener('click', function (x) {
+    x.preventDefault();
+    if (password < than 6) {
+        alert ('Password must be at least 6 characters long'); return false;
+    }else{'submitted'}
+    }
+});
