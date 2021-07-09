e1eff51f25a3fc178c9bb6df2f0a70adec8d6f1f
Maryann878 <85343201+Maryann878@users.noreply.github.com>
2021-07-09 11:58:45 +0100

    Add files via upload

--- object.js
+++ object.js
@@ -95,23 +95,32 @@
     
 // })
 
-
-
+const ul = document.getElementById('users')
 
 
 fetch('https://randomuser.me/api/?results=10')
 .then((resp) => resp.json())
 .then(function (data) {
-    console.log(data);
+    let users = data.results;
+    return users.map((user) =>{
+     const li =  document.createElement('li');
+     const img =  document.createElement('img');
+     const p =  document.createElement('p');
+
+     ul.appendChild(li);
+     let paragraph = li.appendChild(p);
+     let image = li.appendChild(img);
+     let firstName = user.name.first;
+     let lastName = user.name.last;
+     console.log(user.picture.large);
+    //  paragraph.innerHTML = firstname + " " lastname)
+     paragraph.innerHTML = `${firstName} ${lastName}`;
+     image.src = user.picture.large;
+
+    });
 })
     .catch(function (error) {
         console.log(error);
     });
 
-    // let data = {name:'Michael Angelo',}
-    
-
-    // let candidates.map(functionn (author){}
-     candidates.map(functionn (author){}
-
-
+   
